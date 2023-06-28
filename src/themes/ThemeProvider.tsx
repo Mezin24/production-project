23246034db2c79ae9d3b4dcbdb78c32c6a891f0b
Pavel Mezencev <mezencev24@mail.ru>
2023-06-28 16:21:59 +0300

    7. add themes with context

---
+++ src/themes/ThemeProvider.tsx
@@ -0,0 +1,15 @@
+import { FC, useMemo, useState } from "react"
+import { LOCAL_STORAGE_KEY_NAME, Theme, ThemeContext } from "./ThemeContext"
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_KEY_NAME) as Theme || Theme.LIGHT
+
+export const ThemeProvider: FC = ({children}) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const value = useMemo(() => ({
+    theme,
+    setTheme
+  }), [theme])
+  
+  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+}
