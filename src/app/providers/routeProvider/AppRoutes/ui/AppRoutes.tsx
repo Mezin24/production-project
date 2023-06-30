d527506254e71642aa944a2d3f9bf9f0a9957d34
Pavel Mezencev <mezencev24@mail.ru>
2023-06-30 11:41:31 +0300

    20. add page loader widget and not found routes

--- src/app/providers/routeProvider/AppRoutes/ui/AppRoutes.tsx
+++ src/app/providers/routeProvider/AppRoutes/ui/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 
 export const AppRoutes = () => (
-  <Suspense fallback={<h1>Loading...</h1>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {Object.values(routeConfig)
         .map(({ element, path }) => (
