254215bf9632e13241cc9a93e2fe1edbeac8d8a0
Pavel Mezencev <mezencev24@mail.ru>
2023-07-05 10:15:43 +0300

    30.1 add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import { render } from 'react-dom';
 import App from 'app/App';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'app/providers/themeProvider/ui/ThemeProvider';
-import './shared/config/i18nConfig/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary/ui/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
+import { ThemeProvider } from 'app/providers/themeProvider/ui/ThemeProvider';
 import 'app/styles/index.scss';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './shared/config/i18nConfig/i18n';
 
 render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </ErrorBoundary>,
+  <StoreProvider>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </StoreProvider>,
   document.getElementById('app'),
 );
