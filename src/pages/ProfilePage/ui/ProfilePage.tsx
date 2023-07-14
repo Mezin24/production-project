1bde264698968f1b1f620703d92dc0ee29b60474
Pavel Mezencev <mezencev24@mail.ru>
2023-07-14 11:23:50 +0300

    39. Profile module

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,9 +1,12 @@
-import { profileReducer } from 'entities/Profile';
+import { ProfileCard, fetchProfileData, profileReducer } from 'entities/Profile';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
   DynamicModuleLoader,
   ReducerList
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 interface ProfilePageProps {
   className?: string;
@@ -13,12 +16,21 @@ const reducers: ReducerList = {
   profile: profileReducer
 };
 
-const ProfilePage = ({ className }: ProfilePageProps) => (
-  <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-    <div className={classNames('', {}, [className])}>
-      Profile Page
-    </div>
-  </DynamicModuleLoader>
-);
+const ProfilePage = ({ className }: ProfilePageProps) => {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation('profile');
+
+  useEffect(() => {
+    dispatch(fetchProfileData());
+  }, [dispatch]);
+
+  return (
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <div className={classNames('', {}, [className])}>
+        <ProfileCard />
+      </div>
+    </DynamicModuleLoader>
+  );
+};
 
 export default ProfilePage;
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,10 @@
 type Mods = Record<string, string | boolean>
 
-export const classNames = (cls: string, mods: Mods = {}, others: string[] = []): string => [
+export const classNames = (
+  cls: string,
+  mods: Mods = {},
+  others: Array<string | undefined> = []
+): string => [
   cls,
   ...others.filter(Boolean),
   ...Object.entries(mods)
