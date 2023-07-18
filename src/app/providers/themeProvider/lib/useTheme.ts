c916a8cc6d2540596c19145efb023fabb078908b
Pavel Mezencev <mezencev24@mail.ru>
2023-07-18 11:00:11 +0300

    update screenshots and add orange theme

--- src/app/providers/themeProvider/lib/useTheme.ts
+++ src/app/providers/themeProvider/lib/useTheme.ts
@@ -10,7 +10,23 @@ export function useTheme(): UseThemeResult {
   const { setTheme, theme } = useContext(ThemeContext);
 
   const toggle = () => {
-    const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+    let newTheme:Theme;
+
+    switch (theme) {
+    case Theme.LIGHT:
+      newTheme = Theme.DARK;
+      break;
+    case Theme.DARK:
+      newTheme = Theme.ORANGE;
+      break;
+    case Theme.ORANGE:
+      newTheme = Theme.LIGHT;
+      break;
+    default:
+      newTheme = Theme.LIGHT;
+      break;
+    }
+
     localStorage.setItem(LOCAL_STORAGE_KEY_NAME, newTheme);
     setTheme?.(newTheme);
     document.body.className = newTheme;
