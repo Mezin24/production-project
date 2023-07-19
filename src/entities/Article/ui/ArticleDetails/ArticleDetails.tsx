7c5a741faade9685a85f1d540537f14cff8941ee
Pavel Mezencev <mezencev24@mail.ru>
2023-07-19 10:30:51 +0300

    49. add skeleton and loading template in article details

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -10,6 +10,14 @@ import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import {
   fetchArticleById
 } from 'entities/Article/model/services/fetchArticleById/fetchArticleById';
+import { useSelector } from 'react-redux';
+import {
+  getArticleDetailsData,
+  getArticleDetailsError,
+  getArticleDetailsIsLoading
+} from 'entities/Article/model/selectors/getArticleDetails/getArticleDetails';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 import cls from './ArticleDetails.module.scss';
 
 interface ArticleDetailsProps {
@@ -25,15 +33,42 @@ export const ArticleDetails: FC<ArticleDetailsProps> = (props) => {
   const { id, className } = props;
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
+  const isLoading = true;
+  // const isLoading = useSelector(getArticleDetailsIsLoading);
+  const article = useSelector(getArticleDetailsData);
+  const error = useSelector(getArticleDetailsError);
 
   useEffect(() => {
     dispatch(fetchArticleById(id));
   }, [dispatch, id]);
 
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div>
+        <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
+        <Skeleton className={cls.title} width={300} height={32} />
+        <Skeleton className={cls.skeleton} width={600} height={24} />
+        <Skeleton className={cls.skeleton} width="100%" height={200} />
+        <Skeleton className={cls.skeleton} width="100%" height={200} />
+      </div>
+    );
+  } else if (error) {
+    content = (
+      <Text
+        align={TextAlign.CENTER}
+        title={t('Произошла ошибка при загрузке статьи')}
+      />
+    );
+  } else {
+    content = <div>ArticleDetails</div>;
+  }
+
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <div className={classNames(cls.articleDetails, {}, [className])}>
-        ArticleDetails
+        {content}
       </div>
     </DynamicModuleLoader>
   );
